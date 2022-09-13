7995efd6ab52a963748d263118baeb0781cf0266
TomKingori <tomkingori66@gmail.com>
2022-09-13 19:05:11 +0300

    sidebar header

--- src/pages/Inbox.js
+++ src/pages/Inbox.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Chat from '../images/chat.svg'
+import More from '../images/more_vert.svg'
 
 function Inbox(props) {
     return (
         <div className={`inbox rel`}>
-            Inbox / Sidebar
+            {/* Header */}
+            <div className={`userbox flex`}>
+                <div className={`you rel flex aic`}>
+                    <Link to={``} className={`user`}>
+                        <img src={`https://placeimg.com/50/50/people`} alt="profile"/>
+                    </Link>
+                </div>
+                <div className={`actions rel flex aic`}>
+                    <button><img src={Chat} className={`s24`} alt="chat"/></button>
+                    <button><img src={More} className={`s24`} alt="more"/></button>
+                </div>
+            </div>
+            
         </div>
     );
 }
